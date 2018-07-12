6aa5a646428fd415ebee7e89db8e24cc2bfd38ed
Jose Loarca <joseloarca97@icloud.com>
2018-07-12 13:17:06 -0600

    operaciones para vendedores

---
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Product;
+use App\Seller;
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class SellerProductController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Seller $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Seller $seller)
+    {
+        $products = $seller->products;
+
+        return $this->showAll($products);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param User $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, User $seller)
+    {
+        $validation_rules = [
+            'name' => 'required',
+            'description' => 'required',
+            'quantity' => 'required|integer|min:1',
+            'image' => 'required|image'
+        ];
+
+        $this->validate($request, $validation_rules);
+
+        $product_data = $request->all();
+
+        $product_data['status'] = Product::NO_DISPONIBLE;
+        $product_data['image'] = '2.jpg';
+        $product_data['seller_id'] = $seller->id;
+
+        $product = Product::create($product_data);
+
+        return $this->showOne($product, 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Seller $seller
+     * @param Product $product
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Seller $seller, Product $product)
+    {
+        $validation_rules = [
+            'quantity' => 'integer|min:1',
+            'status' => 'in: ' . Product::NO_DISPONIBLE . ',' . Product::DISPONIBLE,
+            'image' => 'image'
+        ];
+
+        $this->validate($request, $validation_rules);
+
+        $this->verificarVendedor($seller, $product);
+
+        $product->fill($request->only([
+            'name',
+            'description',
+            'quantity'
+        ]));
+
+        if ($request->has('status')) {
+            $product->status = $request->status;
+
+            if ($product->isDisponible() && $product->categories()->count() == 0) {
+                return $this->errorResponse('Un producto activo debe tener una categoría asignada', 409);
+            }
+        }
+
+        if ($product->isClean()) {
+            return $this->errorResponse('Se debe especificar al menos un valor diferente para actualizar', 422);
+        }
+
+        $product->save();
+
+        return $this->showOne($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Seller $seller, Product $product)
+    {
+        $this->verificarVendedor($seller, $product);
+
+        $product->delete();
+
+        return $this->showOne($product);
+    }
+
+    protected function verificarVendedor(Seller $seller, Product $product) {
+        if ($seller->id != $product->seller_id) {
+            throw new HttpException(422, 'El vendedor especificado no es el propietario original del producto');
+        }
+    }
+}
