b7c9b25f28b1c6c95e4cfc7b193090cb546c40a9
Jose Loarca <joseloarca97@icloud.com>
2018-07-19 16:18:21 -0600

    cache de resultados

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -5,6 +5,7 @@ namespace App\Traits;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser
@@ -53,6 +54,7 @@ trait ApiResponser
         $collection = $this->sortData($collection, $transformer);
         $collection = $this->paginate($collection);
         $collection = $this->transformData($collection, $transformer);
+        $collection = $this->cacheResponse($collection);
 
         return $this->successResponse($collection, $code);
     }
@@ -166,5 +168,27 @@ trait ApiResponser
         $transformation = fractal($data,  new $transformer);
 
         return $transformation->toArray();
+    }
+
+    /**
+     * Realiza el cache de una peticion
+     *
+     * @param $data
+     * @return mixed
+     */
+    protected function cacheResponse($data)
+    {
+        $url = request()->url();
+        $queryParams = request()->query();
+
+        ksort($queryParams);
+
+        $queryString = http_build_query($queryParams);
+
+        $fullUrl = "{$url}?{$queryString}";
+
+        return Cache::remember($fullUrl, 30/60, function () use ($data){
+            return $data;
+        });
     }
 }
